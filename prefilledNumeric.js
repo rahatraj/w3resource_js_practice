bc3153ff38a346293dfaccfb87c9eee425164eb8
Rahat Reza <rahatreza3199@gmail.com>
2023-08-14 23:49:04 +0530

    prefilled numeric

---
+++ prefilledNumeric.js
@@ -0,0 +1,8 @@
+function prefilledNumeric(num,value){
+    let array =[];
+    for(var i=0; i<num; i++){
+        array.push(value)
+    }
+    return array;
+}
+console.log(prefilledNumeric(6,"rahat"))
