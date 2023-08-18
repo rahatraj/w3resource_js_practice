1fa70c97e63fd0691aa143c90f0ec102963e0a5e
Rahat Reza <rahatreza3199@gmail.com>
2023-08-18 23:43:37 +0530

    greatest Lowest

---
+++ greatestLowest.js
@@ -0,0 +1,12 @@
+function secondGreatestLongest(arr){
+    arm_num.sort(function(x,y){
+        return x-y;
+    });
+    var uniqa = [arr_num[0]];
+    var result = [];
+
+    for(var j=1; j<arr_num.length; j++)
+    {
+       
+    }
+}
