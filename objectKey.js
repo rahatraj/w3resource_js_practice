3d2a8548da5e8abf6ac2872e38eeec02f5767211
Rahat Reza <rahatreza3199@gmail.com>
2023-08-18 23:53:16 +0530

    object key

---
+++ objectKey.js
@@ -0,0 +1,9 @@
+function keys(obj){
+    let varr=[];
+    for(let key in obj){
+          if(key===key)
+          var[key]=key
+    }
+    console.log(varr)
+}
+keys({name:"rahat",classs:"vi",rollno:12})
